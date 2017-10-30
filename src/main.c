181350f559b4bf8bae8a3d1a02cb19fddeafaa7e
felixzhuologist <felix.czhu@gmail.com>
2017-10-30 09:48:23 -0700

    identify users by hostname, reimpl NICK

--- src/main.c
+++ src/main.c
@@ -13,36 +13,49 @@
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
+#include <netdb.h>
 
 #include "log.h"
 
 #define MAX_USERS 100
 
 typedef struct user {
+    char *hostname;
     char *nick;
     char *username;
     char *full_name;
 } user;
 
-user *USERS[MAX_USERS];
+user *USERS[MAX_USERS] = {NULL};
 
-// add user to users list. return pointer to new user, or NULL if no more room
-// TODO: check if user with nick already exists
-user *add_user(char *nick) {
+// Create or retrieve existing entry for user with given hostname, and update
+// the user's nick. Return pointer to that user or NULL if 
+user *add_user(char *hostname, char *nick) {
     for (int i = 0; i < MAX_USERS; i++) {
         if (USERS[i] == NULL) {
             user *new_user = malloc(sizeof(user));
+            new_user->nick = malloc(strlen(nick) + 1);
+            new_user->hostname = malloc(strlen(hostname) + 1);
             strcpy(new_user->nick, nick);
+            strcpy(new_user->hostname, hostname);
             USERS[i] = new_user;
+            chilog(DEBUG, "Adding new user with nick: %s", new_user->nick);
             return new_user;
+        } else if (strcmp(USERS[i]->hostname, hostname) == 0) {
+            char *old_nick = USERS[i]->nick;
+            char *new_nick = malloc(strlen(nick) + 1);
+            strcpy(new_nick, nick);
+            chilog(DEBUG, "Updating user's nick from %s to %s", old_nick, new_nick);
+            USERS[i]->nick = new_nick;
+            return USERS[i];
         }
     }
     return NULL;
 }
 
-user *find_user(char *nick) {
+user *find_user(char *hostname) {
     for (int i = 0; i < MAX_USERS; i++) {
-        if (strcmp(USERS[i]->nick, nick) == 0) {
+        if (strcmp(USERS[i]->hostname, hostname) == 0) {
             return USERS[i];
         }
     }
@@ -56,11 +69,6 @@ struct sockaddr_in init_socket(int port) {
     addr.sin_addr.s_addr = INADDR_ANY; // listen on all
     addr.sin_port = htons(port);       // port
     return addr;
-}
-
-void handle_nick_msg(char *nick) {
-    printf("Received nick command with arg: %s\n", nick);
-    add_user(nick);
 }
 
 char *handle_user_msg(char *msg) {
@@ -85,28 +93,37 @@ void accept_user(int port) {
     bind(sockfd, (struct sockaddr *) &server_addr, sizeof(server_addr));
     listen(sockfd, 5);
 
-    struct sockaddr_in cli_addr;
-    socklen_t client_addr_len = sizeof(cli_addr);
-
-    char buffer[256];
     while (1) {
+        struct sockaddr_in cli_addr;
+        socklen_t client_addr_len = sizeof(cli_addr);
+
+        char buffer[256];
+        char client_hostname[100];
         replysockfd = accept(sockfd, (struct sockaddr *) &cli_addr, &client_addr_len);
+
+        if (getnameinfo((struct sockaddr *) &cli_addr, client_addr_len, 
+            client_hostname, 100, NULL, 0, 0) != 0) {
+            perror("Could not get client hostname");
+        }
+
+        chilog(INFO, "Received connection from client: %s", client_hostname);
         read(replysockfd, buffer, 255);
-        printf("Received message: %s\n", buffer);
+        chilog(INFO, "Received message: %s", buffer);
         char *command = strtok(buffer, " ");
         if (strcmp(command, "NICK") == 0) {
-            handle_nick_msg(strtok(NULL, " "));
-        } else if (strcmp(command, "USER") == 0) {
-            char* reply = handle_user_msg(strtok(NULL, " "));
-            if (reply != NULL) {
-                if (send(replysockfd, reply, sizeof(reply), 0) == -1) {
-                    perror("could not send a reply!");
-                }
-            }
-        } else if (strcmp(command, "EXIT") == 0) { // for debugging only
-            break;
+            char *nick = strtok(NULL, " ");
+            add_user(client_hostname, nick);
+        // } else if (strcmp(command, "USER") == 0) {
+        //     char* reply = handle_user_msg(strtok(NULL, " "));
+        //     if (reply != NULL) {
+        //         if (send(replysockfd, reply, sizeof(reply), 0) == -1) {
+        //             perror("could not send a reply!");
+        //         }
+        //     }
+        // } else if (strcmp(command, "EXIT") == 0) { // for debugging only
+        //     break;
         } else {
-            printf("Received unknown command %s\n", command);
+            chilog(WARNING, "Received unknown command %s", command);
         }
         close(replysockfd);
     }
